35ba5ddd624aad684a4b47ac617db3ca1375facd
Anthony Rossi <anro@acm.org>
2010-08-11 18:16:41 -0700

    Added rudimentary admin interface. Fixed configuration so `runserver` will work.

---
+++ collector/admin.py
@@ -0,0 +1,6 @@
+from assignmentcollectorgrader.collector.models import Course, Assignment, Submission
+from django.contrib import admin
+
+admin.site.register(Course)
+admin.site.register(Assignment)
+admin.site.register(Submission)
--- collector/models.py
+++ collector/models.py
@@ -10,13 +10,18 @@ class Course(models.Model):
     term = models.CharField(max_length=6, help_text='The term this course if offered. Valid values are: Fall, Winter, Spring, Summer.')
     
 class Assignment(models.Model):
+    def testfileurl(self, filename):
+        extension = os.path.splitext(filename)
+        return "tests/%s/%s%s/%s%s" % (self.course.coursenum, self.course.term, self.course.year, self.name, extension[1])
+    
     course = models.ForeignKey(Course)
-    name = models.CharField(max_length=25, help_text='No spaces allowed. Exmaple: lab1-linkedlist.')
+    name = models.CharField(max_length=25, help_text='No spaces allowed. Example: lab1-linkedlist.')
     instructions = models.CharField(max_length=255, blank=True)
     start_date = models.DateTimeField(blank=True, help_text='Date and time to begin allowing submission of assignments.')
     due_date = models.DateTimeField(blank=True, help_text='Date and time to stop allowing submission of assignments.')
     passkey = models.CharField(max_length=25, blank=True, verbose_name='Access passkey', help_text='A <i>secret</i> passkey to allow submission access.')
     max_submissions = models.IntegerField(default=0, help_text='Maximum allowed submissions per student. 0 for unlimited.')
+    test_file = models.FileField(upload_to=testfileurl, blank=True)
     
 class Submission(models.Model):
     def fileurl(self, filename):
@@ -28,4 +33,4 @@ class Submission(models.Model):
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(max_length=25)
     file = models.FileField(upload_to=fileurl)
-    submission_time = models.DateTimeField(auto_now_add=true)
+    submission_time = models.DateTimeField(auto_now_add=True)
