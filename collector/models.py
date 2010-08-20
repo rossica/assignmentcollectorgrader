e21b7c8681f34d00eef35ef730fa68f46f346400
Anthony Rossi <anro@acm.org>
2010-08-20 01:06:02 -0700

    Finished uploading form. JAR Validation works primitively.

--- collector/models.py
+++ collector/models.py
@@ -1,12 +1,16 @@
 from django.db import models
-from django.forms import ModelForm, Form
+from django.forms import ModelForm, Form, ValidationError
+from assignmentcollectorgrader.settings import MEDIA_ROOT
 
 
 ##################
 ###   Models   ###
 ##################
+# TODO: Create Generic Assignment and Submssion objects, and sublcass them to make JAR-specific versions
 
 class Course(models.Model):
+    def __unicode__(self):
+        return self.course_num
     course_num = models.CharField("Course Number", max_length=8, unique=True, help_text='For example: CS260.')
     course_title = models.CharField("Course Title", max_length=25, help_text='For example: Data Structures.')
     description = models.CharField(max_length=255, blank=True, verbose_name='Course Description')
@@ -15,10 +19,14 @@ class Course(models.Model):
     term = models.CharField(max_length=6, help_text='The term this course if offered. Valid values are: Fall, Winter, Spring, Summer.')
     
 class Assignment(models.Model):
+    # TODO: Rename to JARAssignment
     def testfileurl(self, filename):
         import os.path
         extension = os.path.splitext(filename)
         return "tests/%s/%s%s/%s%s" % (self.course.course_num, self.course.term, self.course.year, self.name, extension[1])
+    
+    def __unicode__(self):
+        return self.course.__unicode__() + ": " + self.name
     
     course = models.ForeignKey(Course)
     name = models.CharField(max_length=25, help_text='No spaces allowed. Example: lab1-linkedlist.')
@@ -30,10 +38,14 @@ class Assignment(models.Model):
     test_file = models.FileField(upload_to=testfileurl, blank=True)
     
 class Submission(models.Model):
+    # TODO: Rename to JARSubmission
     def fileurl(self, filename):
         import os.path
         extension = os.path.splitext(filename)
         return "submissions/%s/%s%s/%s_%s%s" % (self.assignment.course.course_num, self.assignment.course.term, self.assignment.course.year, self.last_name, self.first_name, extension[1])
+    
+    def __unicode__(self):
+        return self.last_name + " " + self.first_name + ": " + self.assignment.__unicode__() + " " + str(self.id)
     
     assignment = models.ForeignKey(Assignment)
     first_name = models.CharField(max_length=25)
@@ -46,11 +58,38 @@ class Submission(models.Model):
 #################
 
 class SubmissionForm(ModelForm):
-    
+    # TODO: Create a JAR-specific JAR submission form, and a generic Submission form
     #def clean(self):
-    #    pass
+        # Verify max submissions ?
+        # only possible once all other fields are available
+        #path = MEDIA_ROOT + self.instance.fileurl('')
+        #print self.instance.fileurl('') or 'no path to print'
+        #pass
+    
     def clean_file(self):
-        self.cleaned_data['file']
+        import zipfile, os.path
+        
+        f = self.cleaned_data['file']
+        
+        # Verify that the uploaded is a JAR based on magical numbers
+        try:
+            z = zipfile.ZipFile(f)
+            z.testzip()
+        except(zipfile.BadZipfile):
+            raise ValidationError("Must be a JAR file.")
+        #print f.name or 'no name to print'
+        #if not zipfile.is_zipfile(f.name):
+        #z = zipfile.ZipFile(f)
+        #print z.testzip()
+        #if not z.testzip():
+        #    raise ValidationError("Must be a JAR file.")
+        
+        # Verify File extension
+        name, extension = os.path.splitext(f.name)
+        if not extension == '.jar':
+            raise ValidationError("Must be a JAR file.")
+
+        return f
     
     class Meta:
         model = Submission
--- collector/views.py
+++ collector/views.py
@@ -23,8 +23,13 @@ def submit_assignment(request, course_id, assn_name):
     c = get_object_or_404(Course, course_num=course_id)
     assn = get_object_or_404(Assignment, course=c.pk, name=assn_name)
     
+    #if now is later than assn.due_date:
+    #    return and inform the user that submission is closed
+    
     if request.method == 'POST':
-        form = SubmissionForm(request.POST, request.FILES)
+        s = Submission(assignment=assn)
+        form = SubmissionForm(request.POST, request.FILES, instance=s)
+        
         if form.is_valid():
             # either warn the user they have submitted a late assignment
             # or in the validation, check if it is late, and disallow submission
@@ -33,7 +38,7 @@ def submit_assignment(request, course_id, assn_name):
             
             # Once saved, run the tests on the uploaded assignment and save the output as a string
             
-            grader_output = "" # placeholder for grader code
+            grader_output = "Upload Successful!" # placeholder for grader code
             
             return render_to_response('collector/assignment.html', {'assignment':assn, 'form':SubmissionForm(), 'grader_output':grader_output, })
             # Maybe use a Response redirect to prevent students from refreshing the page and resubmitting the same assignment. 
