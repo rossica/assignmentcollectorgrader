7a5604b6843208a73214c3c399506aaf54e315bb
Anthony Rossi <anro@acm.org>
2010-08-22 02:17:00 -0700

    Adding the collector urls.py (somehow it got skipped??) and added an .hgignore to ignore pyc files.

---
+++ collector/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls.defaults import *
+from assignmentcollectorgrader.collector.views import *
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^AssignmentCollectorGrader/', include('AssignmentCollectorGrader.foo.urls')),
+    
+    (r'^$', course_index),
+    (r'^(?P<course_id>[A-Za-z]{1,4}\d{3}[A-Za-z]?)/$', view_course), #
+    (r'^(?P<course_id>[A-Za-z]{1,4}\d{3}[A-Za-z]?)/(?P<assn_name>[A-Za-z0-9_\-]+)/$', view_assignment),
+    (r'^(?P<course_id>[A-Za-z]{1,4}\d{3}[A-Za-z]?)/(?P<assn_name>[A-Za-z0-9_\-]+)/submit/$', submit_assignment),
+)
